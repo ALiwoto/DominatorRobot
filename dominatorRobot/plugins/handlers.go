cb133ce2dc8a6055c39cbc888d6a2e4ab039462d
aliwoto <aminnimaj@gmail.com>
2021-11-16 21:49:33 +0000

    Add handler
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- dominatorRobot/plugins/handlers.go
+++ dominatorRobot/plugins/handlers.go
@@ -2,9 +2,11 @@ package plugins
 
 import (
 	"github.com/AnimeKaizoku/DominatorRobot/dominatorRobot/plugins/scanPlugin"
+	"github.com/AnimeKaizoku/DominatorRobot/dominatorRobot/plugins/startPlugin"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
 func LoadAllHandlers(d *ext.Dispatcher, triggers []rune) {
 	scanPlugin.LoadAllHandlers(d, triggers)
+	startPlugin.LoadAllHandlers(d, triggers)
 }
