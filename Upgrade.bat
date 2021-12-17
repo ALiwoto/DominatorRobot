87c7aabf003dcbbbeed511fb964776d3a3a53ed1
TsunayoshiSawada <animekaizokux@gmail.com>
2021-12-17 11:21:55 +0530

    Removed go mod vendor from upgrade and build bats

--- Upgrade.bat
+++ Upgrade.bat
@@ -1,3 +1,3 @@
 @echo off
-git pull && powershell -command "Stop-service -Force -name "DominatorRobot" -ErrorAction SilentlyContinue; go mod vendor; go build; Start-service -name "DominatorRobot""
+git pull && powershell -command "Stop-service -Force -name "DominatorRobot" -ErrorAction SilentlyContinue; go build; Start-service -name "DominatorRobot""
 :: Hail Hydra
